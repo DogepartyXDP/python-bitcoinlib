7fed5c61a3f296fddc02ec4c23de8072d041c05f
Javier Varona <javier@contimita.com>
2021-07-21 08:37:35 -0400

    - Changing Dogecoin network parameters

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -19,39 +19,42 @@ import bitcoin.core
 __version__ = '0.10.2dev'
 
 class MainParams(bitcoin.core.CoreMainParams):
-    MESSAGE_START = b'\xf9\xbe\xb4\xd9'
-    DEFAULT_PORT = 8333
-    RPC_PORT = 8332
-    DNS_SEEDS = (('bitcoin.sipa.be', 'seed.bitcoin.sipa.be'),
-                 ('bluematt.me', 'dnsseed.bluematt.me'),
-                 ('dashjr.org', 'dnsseed.bitcoin.dashjr.org'),
-                 ('bitcoinstats.com', 'seed.bitcoinstats.com'),
-                 ('xf2.org', 'bitseed.xf2.org'),
-                 ('bitcoin.jonasschnelli.ch', 'seed.bitcoin.jonasschnelli.ch'))
-    BASE58_PREFIXES = {'PUBKEY_ADDR':0,
-                       'SCRIPT_ADDR':5,
-                       'SECRET_KEY' :128}
+    MESSAGE_START = b'\xc0\xc0\xc0\xc0'
+    DEFAULT_PORT = 8335
+    RPC_PORT = 8335
+    DNS_SEEDS = (('multidoge.org', 'seed.multidoge.org'),
+        ('multidoge.org', 'seed2.multidoge.org'),
+        ('denarius.pro','veryseed.denarius.pro'),
+        ('denarius.pro','muchseed.denarius.pro'),
+        ('denarius.pro','suchseed.denarius.pro'),
+        ('dogecoin.com','seed.dogecoin.com'),
+        ('dogechain.info','seed.dogechain.info'),
+        ('mophides.com','seed.mophides.com'),
+        ('dglibrary.org','seed.dglibrary.org'))
+    BASE58_PREFIXES = {'PUBKEY_ADDR':30,
+                       'SCRIPT_ADDR':22,
+                       'SECRET_KEY' :158}    
+    BECH32_HRP = ''
 
 class TestNetParams(bitcoin.core.CoreTestNetParams):
-    MESSAGE_START = b'\x0b\x11\x09\x07'
-    DEFAULT_PORT = 18333
-    RPC_PORT = 18332
-    DNS_SEEDS = (('testnetbitcoin.jonasschnelli.ch', 'testnet-seed.bitcoin.jonasschnelli.ch'),
-                 ('petertodd.org', 'seed.tbtc.petertodd.org'),
-                 ('bluematt.me', 'testnet-seed.bluematt.me'),
-                 ('bitcoin.schildbach.de', 'testnet-seed.bitcoin.schildbach.de'))
-    BASE58_PREFIXES = {'PUBKEY_ADDR':111,
-                       'SCRIPT_ADDR':196,
-                       'SECRET_KEY' :239}
-
-class RegTestParams(bitcoin.core.CoreRegTestParams):
-    MESSAGE_START = b'\xfa\xbf\xb5\xda'
-    DEFAULT_PORT = 18444
-    RPC_PORT = 18443
+    MESSAGE_START = b'\xfc\xc1\xb7\xdc'
+    DEFAULT_PORT = 18335
+    RPC_PORT = 18335
     DNS_SEEDS = ()
     BASE58_PREFIXES = {'PUBKEY_ADDR':111,
                        'SCRIPT_ADDR':196,
                        'SECRET_KEY' :239}
+    BECH32_HRP = ''
+
+class RegTestParams(bitcoin.core.CoreRegTestParams):
+    MESSAGE_START = b'\xfc\xc1\xb7\xdc'
+    DEFAULT_PORT = 18335
+    RPC_PORT = 18335
+    DNS_SEEDS = ()
+    BASE58_PREFIXES = {'PUBKEY_ADDR':111,
+                       'SCRIPT_ADDR':196,
+                       'SECRET_KEY' :239}
+    BECH32_HRP = ''
 
 """Master global setting for what chain params we're using.
 
